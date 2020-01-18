2487a3cee67a8173ef2fa27c87b0833ebcdab663
Danny <windate3411@gmail.com>
2020-01-18 14:12:57 +0800

    add append function

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="calculator">
-    <div class="display">{{result || '0'}}</div>
-    <div class="btn" @click="result=0">C</div>
+    <div class="display">{{current || '0'}}</div>
+    <div class="btn" @click="current='0'">C</div>
     <div class="btn" @click="sign">+/-</div>
     <div class="btn" @click="percent">%</div>
     <div class="btn operator">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div class="btn" @click="append('7')">7</div>
+    <div class="btn" @click="append('8')">8</div>
+    <div class="btn" @click="append('9')">9</div>
     <div class="btn operator">X</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
+    <div class="btn" @click="append('4')">4</div>
+    <div class="btn" @click="append('5')">5</div>
+    <div class="btn" @click="append('6')">6</div>
     <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div class="btn" @click="append('1')">1</div>
+    <div class="btn" @click="append('2')">2</div>
+    <div class="btn" @click="append('3')">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
+    <div class="btn zero" @click="append('0')">0</div>
     <div class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
@@ -28,18 +28,22 @@ export default {
   name: "Calculator",
   data() {
     return {
-      result: "1234"
+      current: "1234"
     };
   },
   methods: {
     sign() {
-      this.result =
-        this.result.charAt(0) === "-"
-          ? this.result.slice(1)
-          : `-${this.result}`;
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
     },
     percent() {
-      this.result = `${parseFloat(this.result) / 100}`;
+      this.result = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      this.current =
+        this.current === "0" ? `${number}` : `${this.current}${number}`;
     }
   }
 };
